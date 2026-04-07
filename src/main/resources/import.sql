487df683fd65f1df1bb4e47995c03cea7caa32cf
beawata <beawata.dev@gmail.com>
2026-04-07 15:09:00 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$2tQ4G95fE6EBDmrh56Y6FuDrM6p3zL4D1PDxXoBGmvfS3mpzIK8Fa');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$2tQ4G95fE6EBDmrh56Y6FuDrM6p3zL4D1PDxXoBGmvfS3mpzIK8Fa');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
